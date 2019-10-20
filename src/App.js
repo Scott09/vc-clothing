6c5b917eee53e433918de79390515b1973a3fcaf
Scott Appleton <scottappleton09@gmail.com>
2019-10-20 02:39:30 -0700

    add initial homepage menu items and some styling using sass

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import HomePage from './HomePage.component';
 
 function App() {
   return (
     <div>
-      Hello World
+      <HomePage />
     </div>
   );
 }
