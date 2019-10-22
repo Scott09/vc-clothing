7f8f6042337c222857d7ab84bb92bf1feb4717d9
Scott Appleton <scottappleton09@gmail.com>
2019-10-22 15:02:06 -0700

    map over our collections and render out the collection preview of our current items

--- src/App.js
+++ src/App.js
@@ -2,19 +2,14 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import { Switch, Route } from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
 
-
-const HatsPage = () => {
-  return(
-    <div>HATS PAGE</div>
-  )
-}
 
 function App() {
   return (
     <div>
       <Route exact path="/" component={HomePage} />
-      <Route exact path="/shop/hats" component={HatsPage} />
+      <Route exact path="/shop" component={ShopPage} />
     </div>
   );
 }
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import SHOP_DATA from './shop.data';
+import SHOP_DATA from './shop.data.js';
+
+
+import CollectionPreview from '../../components/collection-preview/collection-preview.component'; 
 
 class ShopPage extends React.Component {
   constructor(props) {
@@ -7,12 +10,19 @@ class ShopPage extends React.Component {
 
     this.state = {
       collections: SHOP_DATA
-    }
+    };
   }
 
   render() {
-    return <div>SHOP PAGE</div>
+    const { collections } = this.state;
+    return <div className="shop-page">
+      {
+        collections.map((collection) => {
+          return <CollectionPreview key={collection.id} title={collection.title} items={collection.items} />
+        })
+      }
+    </div>
   }
 }
 
-export default ShopPage;
+export default ShopPage;  
