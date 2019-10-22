81009f99ff45b277fe6e01a452aa30573717ef09
Scott Appleton <scottappleton09@gmail.com>
2019-10-22 15:04:25 -0700

    use a filter so that only 4 collection items show up in each collection preview

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -8,7 +8,7 @@ const CollectionPreview = ({ title, items}) => {
       <h1 className="title">{title.toUpperCase()}</h1>
       <div className="preview">
         {
-          items.map((item) => {
+          items.filter((item, index) => index < 4).map((item) => {
             return <div key={item.id}>{item.name}</div>
           })
         }
