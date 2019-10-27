43a09ff7c2364cd812a829595662789a8ee4d2f9
Scott Appleton <scottappleton09@gmail.com>
2019-10-27 13:23:27 -0700

    add Contact Page component and Route. Also remove Switch import and component from App for now

---
+++ src/pages/contact/contact.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ContactPage = () => {
+  return (
+    <main className="contact-page">
+      <h1>Contact Us!</h1>
+    </main>
+  );
+}
+
+export default ContactPage;
