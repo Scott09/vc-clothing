404e292f30cc844bee14eda7ea598975560bad7a
Scott Appleton <scottappleton09@gmail.com>
2019-10-29 14:28:26 -0700

    Created SignUp component with controlled states of inputs as well as a handleSubmit function that creates a new user in our firestore with email and password

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import './sign-in-and-sign-out.styles.scss';
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 
 
 const signInAndSignUpPage = () => {
   return (
     <div className="sign-in-and-sign-up">
       <SignIn />
+      <SignUp />
     </div>
   );
 }
