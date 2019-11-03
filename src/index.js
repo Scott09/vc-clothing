03911799f666ade03f28471ed9516579218d26ee
Scott Appleton <scottappleton09@gmail.com>
2019-11-03 12:46:35 -0800

    create store and pass into provider so that app will be able to use

--- src/index.js
+++ src/index.js
@@ -6,8 +6,10 @@ import { Provider } from 'react-redux';
 
 import { BrowserRouter } from 'react-router-dom';
 
+import store from './redux/store';
+
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
