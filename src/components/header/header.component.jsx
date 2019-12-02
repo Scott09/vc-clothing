53de1dee57d9e6709c95521a73d188d5b8ef17a6
Scott Appleton <scottappleton09@gmail.com>
2019-12-01 16:24:10 -0800

    Bring in svg for cart icon. Create CartIcon component and style it with cart count tally overtop and in center of icon

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import  { ReactComponent as Logo } from '../../assets/crown.svg';
 import { connect } from 'react-redux';
+import CartIcon from '../cart-icon/cart-icon.component';
 
 import { auth } from '../../firebase/firebase.utils'
 
@@ -31,7 +32,7 @@ const Header = ({ currentUser }) => {
           </Link>)
           
         }
-        
+        <CartIcon />
       </div>
     </header>
   );
