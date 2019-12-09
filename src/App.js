4bc08ebec3484102cea4088b7ffded67449c23e5
Scott Appleton <scottappleton09@gmail.com>
2019-12-09 02:06:38 -0800

    delete yarn lock file in order to deploy to heroku

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ const mapStateToProps = ({ user }) => ({
 })
 
 const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
+  setCurrentUser: (user) => dispatch(setCurrentUser(user))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
